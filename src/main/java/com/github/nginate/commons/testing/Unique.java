46042d325cafecc854f954f1580218b7bf09025c
Kindrat <legioner.alexei@gmail.com>
2016-05-08 13:51:19 +0300

    license headers

--- src/main/java/com/github/nginate/commons/testing/Unique.java
+++ src/main/java/com/github/nginate/commons/testing/Unique.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright © 2016
+ * Maksim Lozbin <maksmtua@gmail.com>
+ * Oleksii Ihnachuk <legioner.alexei@gmail.com>
+ *
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
 package com.github.nginate.commons.testing;
 
 import lombok.experimental.UtilityClass;
